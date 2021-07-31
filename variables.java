e2fdd4c064f009237c1ba0dcf957e975607d7954
FedorSabeshkin <fsabeshkin@mail.ru>
2021-07-31 11:59:59 +0300

    example of apply variable name rules

---
+++ variables.java
@@ -0,0 +1,48 @@
+1. sum - sumOfCharCodes
+// сумма кодов символов, для вычисления хэша от строки.
+
+
+2. index - indexOfElement
+// индекс элемента в массив
+
+
+3. minIndex - leastUsedIndex
+// индекс с наименьшим кол-ом обращений
+
+
+4. 	inString - sequenceOfParenthesis 
+// Последовательность скобок для валидации
+
+
+5. 	inString - postfixArithmeticExpression
+// алгебраическое выражение в постфиксной записи
+
+
+6.  numbers - numbersForExpression
+// числа из этого выражения
+
+
+7.  beginIndex - positionOfSymbolStart
+	endIndex - positionOfSymbolEnd
+// пара индексов, с помощью которых беру по одному символу из строки 
+
+
+8.  first - firstNumber
+	second - secondNumber
+// числа для алгебраического выражений
+
+
+9. powerSet - unionedSet
+// Множество получившиеся в результате объединения других множеств
+
+
+10. powerSet - setOfIntersection
+// Множество получившиеся в результате пересечения других множеств
+
+
+11. powerSet - setOfDifference
+// Множество получившиеся как разница других множеств
+
+
+12. compareResult - resultOfCompare
+// результат сравнения двух элементов
