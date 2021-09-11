2b0aca065563fc23c9f2a03137726e160dfe9e03
FedorSabeshkin <fsabeshkin@mail.ru>
2021-09-11 23:50:49 +0300

    add second example for comment cases

---
+++ comments_second.java
@@ -0,0 +1,67 @@
+1. // important! we use "sa509" for replacing "=" symbol in query
+
+// Дополнительное предупреждение разработчиков об особенностях синтаксиса
+
+2. //TODO: Get Precision from Metadata
+
+// Добавлена пометка, по которой сможем быстрее найти место для улучшения метода - TODO
+
+3.// TODO: ID is important, need to do it correct
+
+// TODO
+
+4. // Создается map, где ключом является значение фильтра
+   // (поле-предикат-значение), а value - and или or
+
+// Представление намерений
+
+5.// TODO: необходимо использовать предикаты, а не строки, иначе все ломается
+
+// TODO, а так же предупреждение о последствиях
+
+6. 
+if(){
+    // Обычный режим
+}else{
+    // <Type> режим
+}
+
+// Представление намерений
+
+7.
+		// TODO: Get only important types
+
+// TODO
+
+8.
+				/*
+				 * после того как определили typeOfProperty дополнительно преобразуем в сложных
+				 * полях
+				 */
+// Представление намерений
+
+9.
+                /**
+                 * TODO: show modal and ask about close or continue process
+                 */
+
+// TODO
+
+10.
+    /**
+     * TODO: добавить здесь генерацию body по реально хранящимся в БД данным
+     */
+
+// TODO
+
+11.
+    /**
+    * TODO: get number from cookie
+    */
+
+// TODO
+
+12.
+    //TODO: Redirect to url with status code 302
+
+// TODO
